9292809ecc70d29f4f16c13b8dc771265777404a
Shotaro Hirose <hirose.shotaro@outlook.com>
2021-11-30 22:10:24 +0900

    postsコレクションの生成を追加

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 const input = `src`;
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addCollection(`posts`, function(collectionApi) {
+    return collectionApi.getFilteredByGlob(`${input}/posts/*.md`);
+  });
+
   return {
     dir: {
       input: input
